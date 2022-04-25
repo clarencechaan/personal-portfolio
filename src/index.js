d016d82247deca74395a534d26ed3ba42335c9e0
Clarence Chan <clarencechaan@icloud.com>
2022-04-25 19:14:51 -0400

    add auditty project, fix mobile

--- src/index.js
+++ src/index.js
@@ -18,19 +18,30 @@ for (let i = 0; i < 10; i++) {
 // 3D effect on hero
 const hero = document.querySelector(".hero");
 const heroContent = document.querySelector(".hero-content");
-hero.addEventListener("mousemove", (e) => {
-  const xPos = hero.getBoundingClientRect().left + hero.offsetWidth / 2;
-  const yPos =
-    hero.getBoundingClientRect().top + hero.offsetHeight / 2 + window.scrollY;
-  const xDist = e.pageX - xPos;
-  const yDist = e.pageY - yPos;
-  const xDiffPercent = xDist / xPos;
-  const yDiffPercent = yDist / yPos;
-  console.log(xDiffPercent, yDiffPercent);
-  heroContent.style.transform = `perspective(1000px) rotateX(${
-    -yDiffPercent * 10
-  }deg) rotateY(${xDiffPercent * 10}deg) translateZ(100px)`;
-});
+if (!isTouchDevice()) {
+  hero.addEventListener("mousemove", (e) => {
+    const xPos = hero.getBoundingClientRect().left + hero.offsetWidth / 2;
+    const yPos =
+      hero.getBoundingClientRect().top + hero.offsetHeight / 2 + window.scrollY;
+    const xDist = e.pageX - xPos;
+    const yDist = e.pageY - yPos;
+    const xDiffPercent = xDist / xPos;
+    const yDiffPercent = yDist / yPos;
+    console.log(xDiffPercent, yDiffPercent);
+    heroContent.style.transform = `perspective(1000px) rotateX(${
+      -yDiffPercent * 10
+    }deg) rotateY(${xDiffPercent * 10}deg) translateZ(100px)`;
+  });
+}
+
+// don't apply 3D effect on hero if touch screen
+function isTouchDevice() {
+  return (
+    "ontouchstart" in window ||
+    navigator.maxTouchPoints > 0 ||
+    navigator.msMaxTouchPoints > 0
+  );
+}
 
 // slide in on scroll
 window.addEventListener("scroll", () => {
@@ -53,11 +64,11 @@ function techsSlide() {
   for (let i = 0; i < techs.length; i++) {
     if (techDiffPercent >= 0.5) {
       techs[i].style.transform = `translateX(${
-        (techDiffPercent - 0.5) * 25 * (i + 10)
+        (techDiffPercent - 0.5) * 10 * (i + 10)
       }%)`;
     } else if (techDiffPercent <= -0.5) {
       techs[i].style.transform = `translateX(${
-        (techDiffPercent + 0.5) * 25 * (i + 10)
+        (techDiffPercent + 0.5) * 10 * (i + 10)
       }%)`;
     } else {
       techs[i].style.transform = "translateX(0)";
@@ -102,46 +113,50 @@ function projectsSlide() {
       projects[i].getBoundingClientRect().top + projects[i].offsetHeight / 2;
     const projDiffPercent =
       (window.innerHeight / 2 - projectCenter) / projects[i].offsetHeight;
-    if (Math.abs(projDiffPercent) > 1.5) {
-      return;
+    // if (Math.abs(projDiffPercent) > 1.5) {
+    //   return;
+    // }
+    let direction = 1;
+    if (i % 2 !== 0) {
+      direction = -1;
     }
     if (projDiffPercent >= 0.5) {
       projectDescs[i].style.transform = `translateX(${
-        -(projDiffPercent - 0.5) * 125
+        -(projDiffPercent - 0.5) * 75 * direction
       }%)`;
       projectTitles[i].style.transform = `translateX(${
-        -(projDiffPercent - 0.5) * 25
+        -(projDiffPercent - 0.5) * 25 * direction
       }%)`;
       projectDescTexts[i].style.transform = `translateX(${
-        -(projDiffPercent - 0.5) * 50
+        -(projDiffPercent - 0.5) * 50 * direction
       }%)`;
       projectBtns[2 * i].style.transform = `translateX(${
-        -(projDiffPercent - 0.5) * 75
+        -(projDiffPercent - 0.5) * 75 * direction
       }%)`;
       projectBtns[2 * i + 1].style.transform = `translateX(${
-        -(projDiffPercent - 0.5) * 100
+        -(projDiffPercent - 0.5) * 100 * direction
       }%)`;
       projectVids[i].style.transform = `translateX(${
-        -(projDiffPercent - 0.5) * 50
+        -(projDiffPercent - 0.5) * 25 * direction
       }%)`;
     } else if (projDiffPercent <= -0.5) {
       projectDescs[i].style.transform = `translateX(${
-        -(projDiffPercent + 0.5) * 125
+        -(projDiffPercent + 0.5) * 75 * direction
       }%)`;
       projectTitles[i].style.transform = `translateX(${
-        -(projDiffPercent + 0.5) * 25
+        -(projDiffPercent + 0.5) * 25 * direction
       }%)`;
       projectDescTexts[i].style.transform = `translateX(${
-        -(projDiffPercent + 0.5) * 50
+        -(projDiffPercent + 0.5) * 50 * direction
       }%)`;
       projectBtns[2 * i].style.transform = `translateX(${
-        -(projDiffPercent + 0.5) * 75
+        -(projDiffPercent + 0.5) * 75 * direction
       }%)`;
       projectBtns[2 * i + 1].style.transform = `translateX(${
-        -(projDiffPercent + 0.5) * 100
+        -(projDiffPercent + 0.5) * 100 * direction
       }%)`;
       projectVids[i].style.transform = `translateX(${
-        -(projDiffPercent + 0.5) * 50
+        -(projDiffPercent + 0.5) * 25 * direction
       }%)`;
     } else {
       projectDescs[i].style.transform = "translateX(0)";
