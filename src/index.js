4158751636fe119020d20323a2bacafb284e7f48
Clarence Chan <clarencechaan@icloud.com>
2022-04-26 15:35:16 -0400

    on-page navigation

--- src/index.js
+++ src/index.js
@@ -43,182 +43,19 @@ function isTouchDevice() {
   );
 }
 
-// slide in on scroll
-window.addEventListener("scroll", () => {
-  techsSlide();
-  bioSlide();
-  projectsSlide();
-  contactSlide();
-});
-
-const technologies = document.querySelector(".technologies");
-const techs = document.querySelectorAll(".tech");
-
-function techsSlide() {
-  const technologiesCenter =
-    technologies.getBoundingClientRect().top + technologies.offsetHeight / 2;
-  const techDiffPercent =
-    (window.innerHeight / 2 - technologiesCenter) / technologies.offsetHeight;
-  if (Math.abs(techDiffPercent) > 1.5) {
-    return;
-  }
-  for (let i = 0; i < techs.length; i++) {
-    if (techDiffPercent >= 0.5) {
-      techs[i].style.transform = `translateX(${
-        (techDiffPercent - 0.5) * 10 * (i + 10)
-      }%)`;
-    } else if (techDiffPercent <= -0.5) {
-      techs[i].style.transform = `translateX(${
-        (techDiffPercent + 0.5) * 10 * (i + 10)
-      }%)`;
-    } else {
-      techs[i].style.transform = "translateX(0)";
+function highlightTab(id) {
+  const tabs = document.querySelectorAll(".nav-bar a");
+  for (const tab of tabs) {
+    tab.classList.remove("highlighted");
+    if (tab.id === "tab-" + id) {
+      tab.classList.add("highlighted");
     }
   }
 }
 
-const bio = document.querySelector(".bio");
-const codeImg = document.querySelector(".code-img");
-const bioDesc = document.querySelector(".bio-desc");
-
-function bioSlide() {
-  const bioCenter = bio.getBoundingClientRect().top + bio.offsetHeight / 2;
-  const bioDiffPercent =
-    (window.innerHeight / 2 - bioCenter) / bio.offsetHeight;
-  if (Math.abs(bioDiffPercent) > 1.5) {
-    return;
-  }
-  if (bioDiffPercent >= 0.5) {
-    codeImg.style.transform = `translateX(${(bioDiffPercent - 0.5) * 50}%)`;
-    bioDesc.style.transform = `translateX(${(bioDiffPercent - 0.5) * 30}%)`;
-  } else if (bioDiffPercent <= -0.5) {
-    codeImg.style.transform = `translateX(${(bioDiffPercent + 0.5) * 50}%)`;
-    bioDesc.style.transform = `translateX(${(bioDiffPercent + 0.5) * 30}%)`;
-  } else {
-    codeImg.style.transform = "translateX(0)";
-    bioDesc.style.transform = "translateX(0)";
-  }
-}
-
-// slide in on scroll for project section
-const projects = document.querySelectorAll(".project");
-const projectDescs = document.querySelectorAll(".project-desc");
-const projectTitles = document.querySelectorAll(".project-title");
-const projectDescTexts = document.querySelectorAll(".project-desc-text");
-const projectBtns = document.querySelectorAll(".project-desc .main_div");
-const projectVids = document.querySelectorAll(".project > .vid-container");
-
-function projectsSlide() {
-  for (let i = 0; i < projects.length; i++) {
-    const projectCenter =
-      projects[i].getBoundingClientRect().top + projects[i].offsetHeight / 2;
-    const projDiffPercent =
-      (window.innerHeight / 2 - projectCenter) / projects[i].offsetHeight;
-    let direction = 1;
-    if (i % 2 !== 0) {
-      direction = -1;
-    }
-    if (projDiffPercent >= 0.5 && projDiffPercent <= 1.5) {
-      projectDescs[i].style.transform = `translateX(${
-        -(projDiffPercent - 0.5) * 75 * direction
-      }%)`;
-      projectTitles[i].style.transform = `translateX(${
-        -(projDiffPercent - 0.5) * 25 * direction
-      }%)`;
-      projectDescTexts[i].style.transform = `translateX(${
-        -(projDiffPercent - 0.5) * 50 * direction
-      }%)`;
-      projectBtns[2 * i].style.transform = `translateX(${
-        -(projDiffPercent - 0.5) * 75 * direction
-      }%)`;
-      projectBtns[2 * i + 1].style.transform = `translateX(${
-        -(projDiffPercent - 0.5) * 100 * direction
-      }%)`;
-      projectVids[i].style.transform = `translateX(${
-        -(projDiffPercent - 0.5) * 50 * direction
-      }%)`;
-    } else if (projDiffPercent <= -0.5 && projDiffPercent >= -1.5) {
-      projectDescs[i].style.transform = `translateX(${
-        -(projDiffPercent + 0.5) * 75 * direction
-      }%)`;
-      projectTitles[i].style.transform = `translateX(${
-        -(projDiffPercent + 0.5) * 25 * direction
-      }%)`;
-      projectDescTexts[i].style.transform = `translateX(${
-        -(projDiffPercent + 0.5) * 50 * direction
-      }%)`;
-      projectBtns[2 * i].style.transform = `translateX(${
-        -(projDiffPercent + 0.5) * 75 * direction
-      }%)`;
-      projectBtns[2 * i + 1].style.transform = `translateX(${
-        -(projDiffPercent + 0.5) * 100 * direction
-      }%)`;
-      projectVids[i].style.transform = `translateX(${
-        -(projDiffPercent + 0.5) * 50 * direction
-      }%)`;
-    } else {
-      projectDescs[i].style.transform = "translateX(0)";
-      projectTitles[i].style.transform = "translateX(0)";
-      projectDescTexts[i].style.transform = "translateX(0)";
-      projectBtns[2 * i].style.transform = "translateX(0)";
-      projectBtns[2 * i + 1].style.transform = "translateX(0)";
-      projectVids[i].style.transform = "translateX(0)";
-    }
-  }
-}
-
-const contactContent = document.querySelector(".contact-content");
-const contactText = document.querySelector(".contact-text");
-const contactInputs = document.querySelectorAll("form input");
-const contactTextarea = document.querySelector("textarea");
-const contactButton = document.querySelector("form .main_div");
-
-function contactSlide() {
-  const contactCenter =
-    contactContent.getBoundingClientRect().top +
-    contactContent.offsetHeight / 2;
-  const contactDiffPercent =
-    (window.innerHeight / 2 - contactCenter) / contactContent.offsetHeight;
-  if (Math.abs(contactDiffPercent) > 1.5) {
-    return;
-  }
-  if (contactDiffPercent >= 0.5) {
-    contactText.style.transform = `translateX(${
-      (contactDiffPercent - 0.5) * 25
-    }%)`;
-    contactInputs[0].style.transform = `translateX(${
-      (contactDiffPercent - 0.5) * 40
-    }%)`;
-    contactInputs[1].style.transform = `translateX(${
-      (contactDiffPercent - 0.5) * 55
-    }%)`;
-    contactTextarea.style.transform = `translateX(${
-      (contactDiffPercent - 0.5) * 70
-    }%)`;
-    contactButton.style.transform = `translateX(${
-      (contactDiffPercent - 0.5) * 85
-    }%)`;
-  } else if (contactDiffPercent <= -0.5) {
-    contactText.style.transform = `translateX(${
-      -(contactDiffPercent + 0.5) * 25
-    }%)`;
-    contactInputs[0].style.transform = `translateX(${
-      -(contactDiffPercent + 0.5) * 40
-    }%)`;
-    contactInputs[1].style.transform = `translateX(${
-      -(contactDiffPercent + 0.5) * 55
-    }%)`;
-    contactTextarea.style.transform = `translateX(${
-      -(contactDiffPercent + 0.5) * 70
-    }%)`;
-    contactButton.style.transform = `translateX(${
-      -(contactDiffPercent + 0.5) * 85
-    }%)`;
-  } else {
-    contactText.style.transform = "translateX(0)";
-    contactInputs[0].style.transform = "translateX(0)";
-    contactInputs[1].style.transform = "translateX(0)";
-    contactTextarea.style.transform = "translateX(0)";
-    contactButton.style.transform = "translateX(0)";
-  }
+const sections = document.querySelectorAll(".section");
+for (const sec of sections) {
+  sec.onmouseenter = () => {
+    highlightTab(sec.id);
+  };
 }
